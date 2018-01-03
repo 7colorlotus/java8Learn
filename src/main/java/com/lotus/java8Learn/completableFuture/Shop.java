05e82b206a8336b774dc8e7d377b30ea5b97d9b2
zhusheng <zhusheng@ekeyfund.com>
2018-01-03 16:25:04 +0800

    组合异步completeableFuture

---
+++ src/main/java/com/lotus/java8Learn/completableFuture/Shop.java
@@ -0,0 +1,66 @@
+package com.lotus.java8Learn.completableFuture;
+
+import com.lotus.java8Learn.completableFuture.multiTaskFinishByStream.Discount;
+import com.lotus.java8Learn.completableFuture.multiTaskFinishByStream.Quote;
+
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+public class Shop {
+    private String name;
+
+    public Shop() {
+    }
+
+    public Shop(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice(String product) {
+        return calculatePrice(product);
+    }
+
+    private double calculatePrice(String product) {
+        delay();
+        return new Random().nextDouble() * product.charAt(0) + product.charAt(1);
+    }
+
+    public static void delay() {
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Future<Double> getPriceAsync(String product) {
+        CompletableFuture<Double> futurePrice = new CompletableFuture<>();
+        new Thread(() -> {
+            try {
+                double price = calculatePrice(product);
+                futurePrice.complete(price);
+            } catch (Exception ex) {
+                futurePrice.completeExceptionally(ex);
+            }
+        }).start();
+        return futurePrice;
+    }
+
+    public String getPriceForDiscount(String product) {
+        double price = calculatePrice(product);
+        Discount.Code code = Discount.Code.values()[
+                new Random().nextInt(Discount.Code.values().length)];
+        return String.format("%s:%.2f:%s", name, price, code);
+    }
+
+
+}
